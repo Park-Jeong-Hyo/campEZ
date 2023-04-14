203a031a1cf87372bccae37cae3cb1ef0211da6c
mk-2 <dlalsrl234@naver.com>
2023-04-14 14:11:17 +0900

    지도

---
+++ src/main/java/com/campEZ/Project0/web/MapController.java
@@ -0,0 +1,20 @@
+package com.campEZ.Project0.web;
+
+import com.campEZ.Project0.web.form.members.JoinForm;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/map")
+public class MapController {
+  @GetMapping
+  public String Map(){
+    return "/mypage/map";
+  }
+}
