5738c53d8a8247815b84d367bba07bf4c2346660
RTHSO <oraclejava92@gmail.com>
2023-04-23 17:36:41 +0900

    0423/황성옥/유효성검사/html알림창미완성

--- src/main/java/com/campEZ/Project0/web/form/members/JoinForm.java
+++ src/main/java/com/campEZ/Project0/web/form/members/JoinForm.java
@@ -2,24 +2,35 @@ package com.campEZ.Project0.web.form.members;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class JoinForm {
   private String mname;
   @NotBlank
+  @Length(min = 5, max = 20)
+  @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9_-]{5,20}$",
+            message = "아이디는 대소문자를 구분하지 않는 영문자와 숫자를 포함한 20자 이내입니다")
   private String mid;
-  @NotBlank
-  @Size(min=8)
+  @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+  @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}",
+      message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 16자의 비밀번호여야 합니다.")
   private String pw;
-  @NotBlank
-  @Size(min=8)
+  @NotBlank(message = "비밀번호확인은 필수 입력 값입니다.")
+  @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}",
+      message = "비밀번호확인은 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 16자의 비밀번호여야 합니다.")
   private String pwchk;
   @Email
   private String email;
-  @Size(max=10)
+  @NotBlank
+  @Length(min = 3, max = 10)
+  @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z]{3,10}$",
+            message = "닉네임은 한글 또는 영문자로 10자 이내 입니다")
+  @NotBlank
   private String nickname;
+  @NotBlank
   private String phone;
   private String maddress;
   private String mtype;
