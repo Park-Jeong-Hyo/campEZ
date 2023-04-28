4be3a584e6c33535d803b47905740b75cc87d27b
HSO <oraclejava92@gmail.com>
2023-04-28 16:35:04 +0900

    0428/황성옥/캠핑장삭제오류잡음수정완료

--- src/main/java/com/campEZ/Project0/web/RestOrdersController.java
+++ src/main/java/com/campEZ/Project0/web/RestOrdersController.java
@@ -1,6 +1,7 @@
 package com.campEZ.Project0.web;
 
 
+import com.campEZ.Project0.camping.svc.CampingSVC;
 import com.campEZ.Project0.orders.svc.OrdersSVC;
 import com.campEZ.Project0.web.rest.Response;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/orders")
 public class RestOrdersController {
 
+  private final CampingSVC campingSVC;
   private final OrdersSVC ordersSVC;
 
   @GetMapping("/{cnumber}/{area}")
@@ -32,13 +34,17 @@ public class RestOrdersController {
     return response;
   }
 
+  // 캠핑장 삭제
   @GetMapping("/del/{cnumber}")
   public Response<Object> delOrders(
       @PathVariable("cnumber") int cnumber
   ) {
     Response<Object> res = null;
     boolean orderchk = ordersSVC.orderIsExist(cnumber);
+
     if (orderchk == false) {
+      campingSVC.campareaDelete(cnumber);
+      campingSVC.campingDelete(cnumber);
       res =  Response.createRestResponse  ("00", "삭제 가능", orderchk);
     } else {
       res =  Response.createRestResponse("99", "삭제 불가능", orderchk);
